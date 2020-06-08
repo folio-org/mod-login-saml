d3187debdcde88c19e6378f14f2ab6e8c4fb8983
Craig McNally <cmcnally@ebsco.com>
2020-06-08 12:03:42 -0400

    remove //NOSONAR

--- src/main/java/org/folio/config/ConfigurationsClient.java
+++ src/main/java/org/folio/config/ConfigurationsClient.java
@@ -101,7 +101,7 @@ public class ConfigurationsClient {
 
     Promise<SamlConfiguration> result = Promise.promise();
 
-    List<Future> futures = entries.entrySet().stream() //NOSONAR
+    List<Future> futures = entries.entrySet().stream()
       .map(entry -> ConfigurationsClient.storeEntry(headers, entry.getKey(), entry.getValue()))
       .collect(Collectors.toList());
 
