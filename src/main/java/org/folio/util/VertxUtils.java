c971811a0a7328b20accf8062cdb8febd3d9c3f4
craigmcnally <cmcnally@ebsco.com>
2020-06-16 20:08:11 +0000

    test coverage

--- src/main/java/org/folio/util/VertxUtils.java
+++ src/main/java/org/folio/util/VertxUtils.java
@@ -1,9 +1,9 @@
 package org.folio.util;
 
+import java.util.UUID;
+
 import org.folio.session.NoopSession;
-import org.pac4j.core.context.WebContext;
 import org.pac4j.core.context.session.SessionStore;
-import org.pac4j.core.util.CommonHelper;
 import org.pac4j.vertx.VertxWebContext;
 
 import io.vertx.core.Vertx;
@@ -21,56 +21,57 @@ public class VertxUtils {
    * Create a Pac4j {@link VertxWebContext} from {@link RoutingContext} with {@code null} SessionStore
    */
   public static VertxWebContext createWebContext(RoutingContext routingContext) {
-
     return new VertxWebContext(routingContext, new DummySessionStore(routingContext.vertx(), routingContext.session()));
   }
 
-  public static class DummySessionStore implements SessionStore {
+  private VertxUtils() {}
+
+  public static class DummySessionStore implements SessionStore<VertxWebContext>  {
 
     private final Vertx vertx;
     private Session session;
 
     public DummySessionStore(Vertx vertx, Session session) {
       this.vertx = vertx;
-      this.session = session == null ? (Session) new NoopSession() : session;
+      this.session = session == null ? new NoopSession() : session;
     }
 
     @Override
-    public String getOrCreateSessionId(WebContext context) {
-      return session.id();
+    public String getOrCreateSessionId(VertxWebContext context) {
+      String id = session.id();
+      return id != null ? id : "";
     }
 
     @Override
-    public Object get(WebContext context, String key) {
+    public Object get(VertxWebContext context, String key) {
       return session.get(key);
     }
 
     @Override
-    public void set(WebContext context, String key, Object value) {
+    public void set(VertxWebContext context, String key, Object value) {
       session.put(key, value);
     }
 
     @Override
-    public boolean destroySession(WebContext context) {
+    public boolean destroySession(VertxWebContext context) {
       session = null;
       return true;
     }
 
     @Override
-    public Object getTrackableSession(WebContext context) {
+    public Object getTrackableSession(VertxWebContext context) {
       return session;
     }
 
     @Override
-    public SessionStore<?> buildFromTrackableSession(WebContext context, Object trackableSession) {
+    public SessionStore<VertxWebContext> buildFromTrackableSession(VertxWebContext context, Object trackableSession) {
       return new DummySessionStore(vertx, (Session) trackableSession);
     }
 
     @Override
-    public boolean renewSession(WebContext context) {
+    public boolean renewSession(VertxWebContext context) {
       return true;
     }
-
   }
 
 }
---
+++ src/test/java/org/folio/util/VertxUtilsTest.java
@@ -0,0 +1,102 @@
+package org.folio.util;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.log4j.Logger;
+import org.folio.rest.tools.utils.NetworkUtils;
+import org.folio.session.NoopSession;
+import org.folio.util.VertxUtils.DummySessionStore;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.pac4j.core.context.session.SessionStore;
+import org.pac4j.vertx.VertxWebContext;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.auth.PRNG;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.Session;
+import io.vertx.ext.web.sstore.impl.SharedDataSessionImpl;
+
+@RunWith(VertxUnitRunner.class)
+public class VertxUtilsTest {
+
+  public static final Logger logger = Logger.getLogger(VertxUtilsTest.class);
+
+  public static final String KEY = "key";
+  public static final String VALUE = "foo";
+  public static final String VALUE2 = "bar";
+
+  public final Vertx vertx = Vertx.vertx();
+  public final int port = NetworkUtils.nextFreePort();
+  public HttpServer server;
+
+  @Before
+  public void before(TestContext context) {
+    Router router = Router.router(vertx);
+    server = vertx.createHttpServer();
+
+    router.route("/foo")
+      .handler(this::handle);
+
+    server.requestHandler(router::handle)
+      .listen(port, context.asyncAssertSuccess());
+  }
+
+  @After
+  public void after(TestContext context) {
+    server.close(context.asyncAssertSuccess());
+  }
+
+  @Test
+  public void testVertxUtils(TestContext context) {
+    given().get("http://localhost:" + port + "/foo")
+      .then()
+      .statusCode(200);
+  }
+
+  private void handle(RoutingContext rc) {
+    try {
+      Session session = new SharedDataSessionImpl(new PRNG(vertx));
+      session.put(KEY, VALUE);
+
+      SessionStore<VertxWebContext> sessionStore = new DummySessionStore(vertx, null);
+
+      VertxWebContext ctx = VertxUtils.createWebContext(rc);
+      assertNull(sessionStore.get(ctx, KEY));
+      assertEquals("", sessionStore.getOrCreateSessionId(ctx));
+      
+      sessionStore = sessionStore.buildFromTrackableSession(ctx, session);
+      assertEquals(VALUE, sessionStore.get(ctx, KEY));
+
+      sessionStore.set(ctx, KEY, VALUE2);
+      assertEquals(VALUE2, sessionStore.get(ctx, KEY));
+
+      assertNotNull(sessionStore.getOrCreateSessionId(ctx));
+
+      assertTrue(sessionStore.renewSession(ctx));
+
+      assertTrue(sessionStore.destroySession(ctx));
+      assertNull(sessionStore.getTrackableSession(ctx));
+
+      rc.response()
+        .setStatusCode(200)
+        .end();
+    } catch (Throwable t) {
+      logger.error("Unexpected Error", t);
+      rc.response()
+        .setStatusCode(500)
+        .end(t.getMessage());
+    }
+  }
+
+}
