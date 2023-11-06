7bb05d6c1204c61fca03cc6a3684e7e2f7c31ee0
Barbara Löhle <Barbara.Loehle@bsz-bw.de>
2023-11-06 17:00:37 +0100

    MODLOGSAML-111

--- src/test/java/org/folio/util/DataMigrationHelper.java
+++ src/test/java/org/folio/util/DataMigrationHelper.java
@@ -5,47 +5,56 @@ import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.VertxUnitRunner;
+import java.util.concurrent.TimeUnit;
 import java.util.HashMap;
 import java.util.Map;
 import org.folio.config.model.SamlConfiguration;
 import org.folio.dao.ConfigurationsDao;
 import org.folio.dao.impl.ConfigurationsDaoImpl;
-import org.folio.rest.impl.SamlAPITest;
 import org.folio.rest.impl.TestBase;
 import org.folio.util.DataMigrationHelper;
-import org.folio.util.OkapiHelper;
 import org.folio.util.model.OkapiHeaders;
 
 /**
  * @author barbaraloehle
  */
-public class DataMigrationHelper {
+public final class DataMigrationHelper {
   private ConfigurationsDao configurationsDao;
   private Map<String, Header> headerHashMap = new HashMap<String, Header>(3);
-
+  private Map<String, String> parsedHeaders = new HashMap<String, String>(3);  
+    
   public DataMigrationHelper() {}
   
   public DataMigrationHelper(Header tenantHeader, Header tokenHeader, Header okapiUrlHeader) {
     
     configurationsDao = new ConfigurationsDaoImpl();
+    
     headerHashMap.put("tenant", tenantHeader);
     headerHashMap.put("token", tokenHeader);
     headerHashMap.put("okapiUrl", okapiUrlHeader);
-  }
-
-  private Future<SamlConfiguration> dataMigration(Vertx vertx) {
     
-    return configurationsDao.dataMigration(vertx, createOkapiHeaders());
+    parsedHeaders.put(headerHashMap.get("tenant").getName(), headerHashMap.get("tenant").getValue());
+    parsedHeaders.put(headerHashMap.get("token").getName(), headerHashMap.get("token").getValue());
+    parsedHeaders.put(headerHashMap.get("okapiUrl").getName(), headerHashMap.get("okapiUrl").getValue());
   }
 
-  public void dataMigrationCompleted(Vertx vertx, TestContext context) {
+  private Future<SamlConfiguration> dataMigration(Vertx vertx, boolean withDelete) {
+    
+    return configurationsDao.dataMigration(vertx, createOkapiHeaders(), withDelete);
+  }
+
+  public void dataMigrationCompleted(Vertx vertx, TestContext context, boolean withDelete) {
     
     deleteAllConfigurationRecordsCompleted(vertx, context);
     Async asyncDataMigration = context.async();
-    dataMigration(vertx)
+    dataMigration(vertx, withDelete)
       .onComplete(result -> asyncDataMigration.complete());
-    asyncDataMigration.awaitSuccess(20000);
+    
+    asyncDataMigration.awaitSuccess(TimeUnit.MILLISECONDS.convert(10L, TimeUnit.MINUTES));
+  }
+
+  public Map<String, String> getHeaders() {
+    return parsedHeaders;
   }
     
   private static void deleteAllConfigurationRecordsCompleted(Vertx vertx, TestContext context) {
@@ -53,15 +62,10 @@ public class DataMigrationHelper {
     Async asyncDelete = context.async();
     TestBase.deleteAllConfigurationRecords(vertx)
       .onComplete(result -> asyncDelete.complete());
-    asyncDelete.awaitSuccess(20000);
+    asyncDelete.awaitSuccess(TimeUnit.MILLISECONDS.convert(1L, TimeUnit.MINUTES));
   }
 
   private OkapiHeaders createOkapiHeaders() {
-    
-    Map<String, String> parsedHeaders = new HashMap<String, String>(3);
-    parsedHeaders.put(headerHashMap.get("tenant").getName(), headerHashMap.get("tenant").getValue());
-    parsedHeaders.put(headerHashMap.get("token").getName(), headerHashMap.get("token").getValue());
-    parsedHeaders.put(headerHashMap.get("okapiUrl").getName(), headerHashMap.get("okapiUrl").getValue());
     return OkapiHelper.okapiHeaders(parsedHeaders);
   }
 }
