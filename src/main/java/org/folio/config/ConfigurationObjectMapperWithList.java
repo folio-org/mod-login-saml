32fa47aea76e6868e7bb402be6cb30604948aa33
Barbara Löhle <Barbara.Loehle@bsz-bw.de>
2023-11-06 16:57:43 +0100

    MODLOGSAML-111: Additional Class for Mapping a IDs in a List

---
+++ src/main/java/org/folio/config/ConfigurationObjectMapperWithList.java
@@ -0,0 +1,44 @@
+package org.folio.config;
+
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import org.folio.config.model.SamlConfiguration;
+
+import java.util.ArrayList;
+import java.util.stream.Collector;
+
+public class ConfigurationObjectMapperWithList {
+
+  // prevent instantiating this static util class
+  private ConfigurationObjectMapperWithList() {
+  }
+
+  public static Future<SamlConfiguration> map(JsonArray array, SamlConfiguration samlConfiguration) {
+    try {
+      samlConfiguration.setIdsList(mapInternal(array));
+      return Future.succeededFuture(samlConfiguration);
+    } catch (Exception ex) {
+      return Future.failedFuture(ex);
+    }
+  }
+
+  private static ArrayList<String> mapInternal(JsonArray array) {
+
+    return array.stream()
+      .filter(JsonObject.class::isInstance)
+      .map(JsonObject.class::cast)
+      .collect(toLocalList());
+  }
+
+  private static Collector<JsonObject, ArrayList<String>, ArrayList<String>> toLocalList() {
+
+    return Collector.of(
+      ArrayList<String>::new,
+      (resultSupplier, entry) -> resultSupplier.add(entry.getString("id")),
+      (result, resultSupplier) -> {result.addAll(resultSupplier); return result;}
+    );
+
+  }
+}
+
