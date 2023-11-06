e2afd0cbebb2aab317463c55b51b3092c18920a9
Barbara Löhle <Barbara.Loehle@bsz-bw.de>
2023-11-06 16:56:44 +0100

    MODLOGSAML-111: Additional Class

---
+++ src/test/java/org/folio/rest/impl/TenantRefAPITest.java
@@ -0,0 +1,115 @@
+package org.folio.rest.impl;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.vertx.ext.unit.Async;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.folio.config.SamlConfigHolder;
+import org.folio.rest.client.TenantClient;
+import org.folio.rest.jaxrs.model.TenantAttributes;
+import org.folio.rest.tools.utils.TenantInit;
+import org.folio.util.TenantClientExtended;
+import org.folio.util.MockJson;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+
+/**
+ * @author barabaraloehle
+ */
+@RunWith(VertxUnitRunner.class)
+public class TenantRefAPITest extends TestBase {
+  private static final Logger log = LogManager.getLogger(TenantRefAPITest.class);
+
+  private static final Header TENANT_HEADER = new Header("X-Okapi-Tenant", TENANT);
+  private static final Header TOKEN_HEADER = new Header("X-Okapi-Token", TENANT);
+  private static final Header JSON_CONTENT_TYPE_HEADER = new Header("Content-Type", "application/json");
+
+  private static int JSON_MOCK_PORT = TestBase.MODULE_PORT; //NetworkUtils.nextFreePort();
+  private static final Header OKAPI_URL_HEADER = new Header("X-Okapi-Url", "http://localhost:" + JSON_MOCK_PORT);
+
+  private static MockJson mock = new MockJson();
+  
+  @Rule
+  public TestName testName = new TestName();
+  public final String LOCALHOST_ORIGIN = "http://localhost";
+
+  @BeforeClass
+  public static void setupOnce(TestContext context) {
+    RestAssured.port = TestBase.MODULE_PORT;
+    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+    DeploymentOptions okapiOptions = new DeploymentOptions()
+      .setConfig(new JsonObject().put("http.port", JSON_MOCK_PORT));
+    
+    mock.setMockContent("mock_content_with_delete.json");
+    vertx.deployVerticle(mock, okapiOptions)
+      .onComplete(context.asyncAssertSuccess());
+  }
+
+  @Before
+  public void setUp(TestContext context) {
+    log.info("Running {}", testName.getMethodName());
+  }
+
+  @After
+  public void tearDown() {
+    // Need to clear singleton to maintain test order independence
+    SamlConfigHolder.getInstance().removeClient(TENANT);
+    //deleteAllConfigurationRecords(vertx);
+  } 
+  
+  @Test
+  public void loadDataWithMock(TestContext context) {
+    postTenantExtendedWithTokenCompleted(context);
+    
+    given()
+      .header(TENANT_HEADER)
+      .header(TOKEN_HEADER)
+      .header(OKAPI_URL_HEADER)
+      .header(JSON_CONTENT_TYPE_HEADER)
+      .get("/saml/configuration")
+      .then()
+      .statusCode(200)
+	  
+      .body(matchesJsonSchemaInClasspath("ramls/schemas/SamlConfig.json"))
+      .body("idpUrl", equalTo("https://idp.ssocircle.com"))
+      .body("samlBinding", equalTo("POST"))
+      .body("metadataInvalidated", equalTo(Boolean.FALSE));
+  }
+
+  private void postTenantExtendedWithTokenCompleted(TestContext context) {
+    Async asyncDataMigration = context.async();
+    postTenantExtendedWithToken()
+      .onComplete(result -> asyncDataMigration.complete());
+    asyncDataMigration.awaitSuccess();  
+  }
+
+  public static Future<Void> postTenantExtendedWithToken() {
+    try {
+      TenantAttributes ta = new TenantAttributes();
+      ta.setModuleTo("mod-login-saml-2.0");
+      TenantClient tenantClient = new TenantClientExtended("http://localhost:" + MODULE_PORT, "http://localhost:" + JSON_MOCK_PORT, TENANT, TENANT, webClient);
+      return TenantInit.exec(tenantClient, ta, 6000);
+    } catch (Exception e) {
+      e.printStackTrace(System.err);
+      return Future.failedFuture(e);
+    }
+  }
+
+}
