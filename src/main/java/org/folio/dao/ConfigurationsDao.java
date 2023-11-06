bc4ccd89e3940bef8c0fb902d321c58ac2fd291d
Barbara Löhle <Barbara.Loehle@bsz-bw.de>
2023-11-06 17:03:35 +0100

    MODLOGSAML-111: Additional methods

--- src/main/java/org/folio/dao/ConfigurationsDao.java
+++ src/main/java/org/folio/dao/ConfigurationsDao.java
@@ -9,20 +9,22 @@ import org.folio.util.model.OkapiHeaders;
  * @author barbaraloehle 
  */
 public interface ConfigurationsDao {
-
-	public static final String MODULE_NAME = "LOGIN-SAML";
-	public static final String CONFIG_NAME = "saml";
+  
+  public static final String MODULE_NAME = "LOGIN-SAML";
+  public static final String CONFIG_NAME = "saml";
 	
-	public static final String MISSING_OKAPI_URL = "Missing Okapi URL";
-	public static final String MISSING_TENANT = "Missing Tenant";
-	public static final String MISSING_TOKEN = "Missing Token";
+  public static final String MISSING_OKAPI_URL = "Missing Okapi URL";
+  public static final String MISSING_TENANT = "Missing Tenant";
+  public static final String MISSING_TOKEN = "Missing Token";
 
   
-  public Future<SamlConfiguration> dataMigration(Vertx vertx, OkapiHeaders okapiHeaders);
+  public Future<SamlConfiguration> dataMigration(Vertx vertx, OkapiHeaders okapiHeaders, boolean withDelete);
 
-	public Future<SamlConfiguration> getConfiguration(Vertx vertx, OkapiHeaders okapiHeaders, boolean isPut);
+  public Future<Integer> dataMigrationLoadData(Vertx vertx, OkapiHeaders okapiHeaders, boolean withDelete);
+  
+  public Future<SamlConfiguration> getConfiguration(Vertx vertx, OkapiHeaders okapiHeaders, boolean isPut);
 	
-	public Future<SamlConfiguration> storeSamlConfiguration(Vertx vertx, OkapiHeaders okapiHeaders, SamlConfiguration samlConfiguration);
+  public Future<SamlConfiguration> storeSamlConfiguration(Vertx vertx, OkapiHeaders okapiHeaders, SamlConfiguration samlConfiguration);
 	
-	public Future<SamlConfiguration> storeEntry(Vertx vertx, OkapiHeaders okapiHeaders, String code, String value);
+  public Future<SamlConfiguration> storeEntry(Vertx vertx, OkapiHeaders okapiHeaders, String code, String value);
 }
