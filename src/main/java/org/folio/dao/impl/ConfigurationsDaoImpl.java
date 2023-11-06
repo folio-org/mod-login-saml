6b42311ce50971fde58d9097834b73a032b837a0
Barbara Löhle <Barbara.Loehle@bsz-bw.de>
2023-11-06 17:02:58 +0100

    MODLOGSAML-111: Additional methods

--- src/main/java/org/folio/dao/impl/ConfigurationsDaoImpl.java
+++ src/main/java/org/folio/dao/impl/ConfigurationsDaoImpl.java
@@ -16,18 +16,16 @@ import org.folio.util.model.OkapiHeaders;
 
 import java.util.Objects;
 
-import static java.lang.String.format;
-
 /**
  * @author barbaraloehle 
  */
 public class ConfigurationsDaoImpl implements ConfigurationsDao {
-	private static final Logger LOGGER = LogManager.getLogger(ConfigurationsDaoImpl.class);
+  private static final Logger LOGGER = LogManager.getLogger(ConfigurationsDaoImpl.class);
   private static final String ERROR_MESSAGE_STRING = "errorMessage : %s";
   
-	public static final String CONFIGURATION_TABLE = "configuration";
+  public static final String CONFIGURATION_TABLE = "configuration";
 
-	private static void verifyOkapiHeaders(OkapiHeaders okapiHeaders) throws MissingHeaderException {
+  private static void verifyOkapiHeaders(OkapiHeaders okapiHeaders) throws MissingHeaderException {
     
     if (StringUtils.isBlank(okapiHeaders.getUrl())) {
       throw new MissingHeaderException(MISSING_OKAPI_URL);
@@ -38,91 +36,123 @@ public class ConfigurationsDaoImpl implements ConfigurationsDao {
     if (StringUtils.isBlank(okapiHeaders.getToken())) {
       throw new MissingHeaderException(MISSING_TOKEN);
     }
-	}
-
-	private Future<SamlConfiguration> getConfigurationMigration(Vertx vertx, OkapiHeaders okapiHeaders) {
+  } 
+  
+  private Future<SamlConfiguration> getConfigurationMigration(Vertx vertx, OkapiHeaders okapiHeaders, boolean withDelete) {
     
-		Objects.requireNonNull(okapiHeaders);
-		verifyOkapiHeaders(okapiHeaders);
-		return PostgresClient.getInstance(vertx, okapiHeaders.getTenant())
-	    .get(CONFIGURATION_TABLE, SamlConfiguration.class, new Criterion(), true)
-	    .compose(results -> localFutureGetConfigurationMigration(results, vertx, okapiHeaders));
-	}
+    Objects.requireNonNull(okapiHeaders);
+    verifyOkapiHeaders(okapiHeaders);
+    return PostgresClient.getInstance(vertx, okapiHeaders.getTenant())
+      .get(CONFIGURATION_TABLE, SamlConfiguration.class, new Criterion(), true)
+      .compose(results -> localFutureGetConfigurationMigration(results, vertx, okapiHeaders, withDelete));
+  }
 
-	private Future<SamlConfiguration> localFutureGetConfigurationMigration(Results<SamlConfiguration> results, Vertx vertx, OkapiHeaders okapiHeaders) {
+  private Future<SamlConfiguration> localFutureGetConfigurationMigration(Results<SamlConfiguration> results, Vertx vertx,
+    OkapiHeaders okapiHeaders, boolean withDelete) {
     
-		int localLength = results.getResultInfo().getTotalRecords();
-		if (localLength == 1) {
-	    return Future.succeededFuture(results.getResults().get(0));
-		}
-		if(localLength > 1) {
-	    String errorMessage = String.format("Migration: TotalRecords are not unique. Instead the number is : %s", Integer.toString(localLength));
-	    LOGGER.error(ERROR_MESSAGE_STRING, errorMessage);
-	    return Future.failedFuture(new IllegalArgumentException(errorMessage));
-		}
-		return ConfigurationsClient.getConfiguration(vertx, okapiHeaders)
-	    .compose(result -> storeEntry(vertx, okapiHeaders, result))
+    int localLength = results.getResultInfo().getTotalRecords();
+    if (localLength == 1) {
+      return Future.succeededFuture(results.getResults().get(0));
+    }
+    if(localLength > 1) {
+      String errorMessage = String.format("Migration: TotalRecords are not unique. Instead the number is : %s", Integer.toString(localLength));
+      LOGGER.error(ERROR_MESSAGE_STRING, errorMessage);
+      return Future.failedFuture(new IllegalArgumentException(errorMessage));
+    }
+    return ConfigurationsClient.getConfigurationWithIds(vertx, okapiHeaders)
+      .compose(result -> storeEntry(vertx, okapiHeaders, result))
+      .onSuccess(result -> {
+          if (withDelete) 
+            ConfigurationsClient.deleteConfigurationEntries(vertx, okapiHeaders, result);
+          else
+            Future.succeededFuture(result);
+        })
       .onFailure(cause -> LOGGER.warn("There is an empty local DB : %s", cause.getMessage()));
-	}
+  }
 
   @Override
-  public Future<SamlConfiguration> dataMigration(Vertx vertx, OkapiHeaders okapiHeaders) {
+  public Future<SamlConfiguration> dataMigration(Vertx vertx, OkapiHeaders okapiHeaders, boolean withDelete) {
     
-    return getConfigurationMigration(vertx, okapiHeaders)
+    return getConfigurationMigration(vertx, okapiHeaders, withDelete)
       .onFailure(cause -> {
           String warnMessage = "Local: Cannot load config from mod-configuration. " + cause.getMessage();
           LOGGER.warn(warnMessage);
+        });
+  } 
+
+  private Future<SamlConfiguration> dataMigrationToLoadData(Vertx vertx, OkapiHeaders okapiHeaders, boolean withDelete) {
+    
+    return dataMigration(vertx, okapiHeaders, withDelete)
+      .recover(cause -> {
+          String warnMessage = "Local: Cannot load config from mod-configuration. " + cause.getMessage();
+          LOGGER.warn(warnMessage);
+          return Future.succeededFuture(new SamlConfiguration());
       });
   } 
-	
-	@Override
-	public Future<SamlConfiguration> getConfiguration(Vertx vertx, OkapiHeaders okapiHeaders, boolean isPut) {
+   
+  @Override
+  public Future<Integer> dataMigrationLoadData(Vertx vertx, OkapiHeaders okapiHeaders, boolean withDelete) {
     
-		Objects.requireNonNull(okapiHeaders);
-		verifyOkapiHeaders(okapiHeaders);
-		return PostgresClient.getInstance(vertx, okapiHeaders.getTenant())
-	    .get(CONFIGURATION_TABLE, SamlConfiguration.class, new Criterion(), true)
-	    .compose(results -> localFutureGetConfiguration(results, isPut));
-	}
+    return dataMigrationToLoadData(vertx, okapiHeaders, withDelete)
+      .map(result -> {
+          if(result.getId() == null)
+            return Integer.valueOf(0);
+          else
+            return Integer.valueOf(ConfigurationsClient.samlConfiguration2Map(result).size());
+        })
+      .compose(num -> {
+          LOGGER.info("Number of records loaded num={}", num);
+          return Future.succeededFuture(num);
+        });
+  }
+
+  @Override
+  public Future<SamlConfiguration> getConfiguration(Vertx vertx, OkapiHeaders okapiHeaders, boolean isPut) {
+    
+    Objects.requireNonNull(okapiHeaders);
+    verifyOkapiHeaders(okapiHeaders);
+    return PostgresClient.getInstance(vertx, okapiHeaders.getTenant())
+      .get(CONFIGURATION_TABLE, SamlConfiguration.class, new Criterion(), true)
+      .compose(results -> localFutureGetConfiguration(results, isPut));
+  }
 	
   private Future<SamlConfiguration> localFutureGetConfiguration(Results<SamlConfiguration> results, boolean isPut) {
     
-		int localLength = results.getResultInfo().getTotalRecords();
-		if(localLength == 1) {
-			return Future.succeededFuture(results.getResults().get(0));
-		} else if(localLength == 0) {
-        if (isPut)
-          return Future.succeededFuture(new SamlConfiguration());
-        else
-        {
-          String warnMessage = "There is an empty DB";
-          LOGGER.warn(warnMessage);
-          return Future.failedFuture(warnMessage);
-        }
-		} else {
-	    String errorMessage = String.format("TotalRecords are not unique. The number is : %s", Integer.toString(localLength));
-	    LOGGER.error(ERROR_MESSAGE_STRING, errorMessage);
-	    return Future.failedFuture(new IllegalArgumentException(errorMessage));
-		}
-	}
-   
-	@Override
-	public Future<SamlConfiguration> storeSamlConfiguration(Vertx vertx, OkapiHeaders okapiHeaders, SamlConfiguration samlConfiguration) {
-    
-		Objects.requireNonNull(okapiHeaders);
-		Objects.requireNonNull(samlConfiguration);
-		verifyOkapiHeaders(okapiHeaders);
-		return storeEntry(vertx, okapiHeaders, samlConfiguration);
-	}
-	
-	@Override
-	public Future<SamlConfiguration> storeEntry(Vertx vertx, OkapiHeaders okapiHeaders, String code, String value) {
-    
-		Objects.requireNonNull(okapiHeaders);
-		verifyOkapiHeaders(okapiHeaders);
+    int localLength = results.getResultInfo().getTotalRecords();
+    if(localLength == 1) {
+      return Future.succeededFuture(results.getResults().get(0));
+    } else if (localLength == 0) {
+      if (isPut)
+        return Future.succeededFuture(new SamlConfiguration());
+      else {
+        String warnMessage = "There is an empty DB";
+        LOGGER.warn(warnMessage);
+        return Future.failedFuture(warnMessage);
+      }} else {
+        String errorMessage = String.format("TotalRecords are not unique. The number is : %s", Integer.toString(localLength));
+        LOGGER.error(ERROR_MESSAGE_STRING, errorMessage);
+        return Future.failedFuture(new IllegalArgumentException(errorMessage));
+    }
+  }
 
-		return getConfiguration(vertx, okapiHeaders, false)
-	    .compose(result -> {
+   
+  @Override
+  public Future<SamlConfiguration> storeSamlConfiguration(Vertx vertx, OkapiHeaders okapiHeaders, SamlConfiguration samlConfiguration) {
+    
+    Objects.requireNonNull(okapiHeaders);
+    Objects.requireNonNull(samlConfiguration);
+    verifyOkapiHeaders(okapiHeaders);
+    return storeEntry(vertx, okapiHeaders, samlConfiguration);
+  }
+	
+  @Override
+  public Future<SamlConfiguration> storeEntry(Vertx vertx, OkapiHeaders okapiHeaders, String code, String value) {
+    
+    Objects.requireNonNull(okapiHeaders);
+    verifyOkapiHeaders(okapiHeaders);
+
+    return getConfiguration(vertx, okapiHeaders, false)
+      .compose(result -> {
          switch(code) {
          case SamlConfiguration.ID_CODE: result.setId(value);
            break;
@@ -152,26 +182,27 @@ public class ConfigurationsDaoImpl implements ConfigurationsDao {
            throw new IllegalArgumentException(errorMessage);}
          }
          return storeEntry(vertx, okapiHeaders, result);
-			});
+        });
 	}
     
-	Future<SamlConfiguration> storeEntry(Vertx vertx, OkapiHeaders okapiHeaders, SamlConfiguration samlConfiguration) {
+  Future<SamlConfiguration> storeEntry(Vertx vertx, OkapiHeaders okapiHeaders, SamlConfiguration samlConfiguration) {
     
-		Objects.requireNonNull(okapiHeaders);
-		Objects.requireNonNull(samlConfiguration);
-		verifyOkapiHeaders(okapiHeaders);
+    Objects.requireNonNull(okapiHeaders);
+    Objects.requireNonNull(samlConfiguration);
+    verifyOkapiHeaders(okapiHeaders);
 
-		return PostgresClient.getInstance(vertx, okapiHeaders.getTenant()) 
-	    .upsert(CONFIGURATION_TABLE, samlConfiguration.getId(), samlConfiguration, true)
-		  .map(result -> localStoreEntry(result, samlConfiguration))
-	    .onFailure(e -> LOGGER.error("Configuration Storage failed: {}", e.getMessage()));
-	}
+    return PostgresClient.getInstance(vertx, okapiHeaders.getTenant()) 
+      .upsert(CONFIGURATION_TABLE, samlConfiguration.getId(), samlConfiguration, true)
+      .map(result -> localStoreEntry(result, samlConfiguration))
+      .onFailure(e -> LOGGER.error("Configuration Storage failed: {}", e.getMessage()));
+  }
 	
-	private SamlConfiguration localStoreEntry(String id, SamlConfiguration samlConfiguration) {
+  private SamlConfiguration localStoreEntry(String id, SamlConfiguration samlConfiguration) {
     
-		if(id != null && samlConfiguration.getId() == null) {
-			samlConfiguration.setId(id);
-		}
-		return samlConfiguration;
-	}	
+    if (id != null && samlConfiguration.getId() == null) {
+      samlConfiguration.setId(id);
+    }
+    return samlConfiguration;
+  }
+
 }
