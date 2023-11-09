3390f1918e095947143f744f84bf650c18114da1
Barbara Löhle <Barbara.Loehle@bsz-bw.de>
2023-11-09 09:39:30 +0100

    MODLOGSAML-111: Further corrections

--- src/main/java/org/folio/rest/impl/TenantRefAPI.java
+++ src/main/java/org/folio/rest/impl/TenantRefAPI.java
@@ -1,23 +1,15 @@
 package org.folio.rest.impl;
 
 import java.util.Map;
-import javax.ws.rs.core.Response;
 
-import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Future;
-import io.vertx.core.Handler;
-import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.dao.impl.ConfigurationsDaoImpl;
-import org.folio.config.SamlConfigHolder;//
-import org.folio.config.model.SamlClientComposite;//
 import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.model.TenantAttributes;
-import org.folio.rest.tools.utils.TenantLoading;
 import org.folio.util.OkapiHelper;
 
 public class TenantRefAPI extends TenantAPI {
